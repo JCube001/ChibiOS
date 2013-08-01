3ff7f932a3a66305035087c6defe188cd34cd094
Jacob McGladdery <jacobm117@gmail.com>
2013-08-01 13:07:05 -0400

    Changed the hal template makefile
    Signed-off-by: Jacob McGladdery <jacobm117@gmail.com>

--- os/hal/platforms/MK20D5/platform.mk
+++ os/hal/platforms/MK20D5/platform.mk
@@ -1,19 +1,19 @@
 # List of all the template platform files.
-PLATFORMSRC = ${CHIBIOS}/os/hal/MK20D5/hal_lld.c \
-              ${CHIBIOS}/os/hal/MK20D5/adc_lld.c \
-              ${CHIBIOS}/os/hal/MK20D5/can_lld.c \
-              ${CHIBIOS}/os/hal/MK20D5/ext_lld.c \
-              ${CHIBIOS}/os/hal/MK20D5/gpt_lld.c \
- 			  ${CHIBIOS}/os/hal/MK20D5/i2c_lld.c \
-              ${CHIBIOS}/os/hal/MK20D5/icu_lld.c \
-              ${CHIBIOS}/os/hal/MK20D5/mac_lld.c \
-              ${CHIBIOS}/os/hal/MK20D5/pal_lld.c \
-              ${CHIBIOS}/os/hal/MK20D5/pwm_lld.c \
-              ${CHIBIOS}/os/hal/MK20D5/sdc_lld.c \
-              ${CHIBIOS}/os/hal/MK20D5/serial_lld.c \
-              ${CHIBIOS}/os/hal/MK20D5/spi_lld.c \
-              ${CHIBIOS}/os/hal/MK20D5/uart_lld.c \
-              ${CHIBIOS}/os/hal/MK20D5/usb_lld.c
+PLATFORMSRC = ${CHIBIOS}/os/hal/platforms/MK20D5/hal_lld.c \
+              ${CHIBIOS}/os/hal/platforms/MK20D5/adc_lld.c \
+              ${CHIBIOS}/os/hal/platforms/MK20D5/can_lld.c \
+              ${CHIBIOS}/os/hal/platforms/MK20D5/ext_lld.c \
+              ${CHIBIOS}/os/hal/platforms/MK20D5/gpt_lld.c \
+ 			  ${CHIBIOS}/os/hal/platforms/MK20D5/i2c_lld.c \
+              ${CHIBIOS}/os/hal/platforms/MK20D5/icu_lld.c \
+              ${CHIBIOS}/os/hal/platforms/MK20D5/mac_lld.c \
+              ${CHIBIOS}/os/hal/platforms/MK20D5/pal_lld.c \
+              ${CHIBIOS}/os/hal/platforms/MK20D5/pwm_lld.c \
+              ${CHIBIOS}/os/hal/platforms/MK20D5/sdc_lld.c \
+              ${CHIBIOS}/os/hal/platforms/MK20D5/serial_lld.c \
+              ${CHIBIOS}/os/hal/platforms/MK20D5/spi_lld.c \
+              ${CHIBIOS}/os/hal/platforms/MK20D5/uart_lld.c \
+              ${CHIBIOS}/os/hal/platforms/MK20D5/usb_lld.c
 
 # Required include directories
-PLATFORMINC = ${CHIBIOS}/os/hal/MK20D5
+PLATFORMINC = ${CHIBIOS}/os/hal/platforms/MK20D5
