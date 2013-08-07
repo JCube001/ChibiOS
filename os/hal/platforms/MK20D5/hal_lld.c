fa60ba67a421b9301d4e1ad8da7b24e7eedd6549
Jacob McGladdery <jacobm117@gmail.com>
2013-08-07 11:38:10 -0400

    Rewrote HAL init function to work with new header file
    Signed-off-by: Jacob McGladdery <jacobm117@gmail.com>

--- os/hal/platforms/MK20D5/hal_lld.c
+++ os/hal/platforms/MK20D5/hal_lld.c
@@ -78,104 +78,99 @@ void hal_lld_init(void) {
  */
 void mk20d5_clock_init(void) {
 
-#if 0
     /* Unlock the watchdog and set to allow updates */
-    WDOG_UNLOCK = 0xC520;
-    WDOG_UNLOCK = 0xD928;
-    WDOG_STCTRLH = WDOG_STCTRLH_ALLOWUPDATE_MASK;
+    WDOG->UNLOCK = 0xC520;
+    WDOG->UNLOCK = 0xD928;
+    WDOG->STCTRLH = WDOG_STCTRLH_ALLOWUPDATE_MASK;
 
     /* Enable clocks for always used peripherals */
-    SIM_SCGC6 = SIM_SCGC6_FTFL_MASK |
-                SIM_SCGC6_DMAMUX_MASK |
-                SIM_SCGC6_SPI0_MASK |
-                SIM_SCGC6_I2S_MASK |
-                SIM_SCGC6_CRC_MASK |
-                SIM_SCGC6_USBDCD_MASK |
-                SIM_SCGC6_PDB_MASK |
-                SIM_SCGC6_PIT_MASK |
-                SIM_SCGC6_FTM0_MASK |
-                SIM_SCGC6_FTM1_MASK |
-                SIM_SCGC6_ADC0_MASK |
-                SIM_SCGC6_RTC_MASK;
+    SIM->SCGC6 = SIM_SCGC6_FTFL_MASK |
+                 SIM_SCGC6_DMAMUX_MASK |
+                 SIM_SCGC6_SPI0_MASK |
+                 SIM_SCGC6_I2S_MASK |
+                 SIM_SCGC6_CRC_MASK |
+                 SIM_SCGC6_USBDCD_MASK |
+                 SIM_SCGC6_PDB_MASK |
+                 SIM_SCGC6_PIT_MASK |
+                 SIM_SCGC6_FTM0_MASK |
+                 SIM_SCGC6_FTM1_MASK |
+                 SIM_SCGC6_ADC0_MASK |
+                 SIM_SCGC6_RTC_MASK;
 
     /* If the RTC oscillator is not enabled, get it started now */
-    if (!(RTC_CR & RTC_CR_OSCE_MASK)) {
-        RTC_SR = 0;
-        RTC_CR = RTC_CR_SC16P_MASK | RTC_CR_SC4P_MASK | RTC_CR_OSCE_MASK;
+    if (!(RTC->CR & RTC_CR_OSCE_MASK)) {
+        RTC->SR = 0;
+        RTC->CR = RTC_CR_SC16P_MASK | RTC_CR_SC4P_MASK | RTC_CR_OSCE_MASK;
     }
 
     /* Release I/O pins hold, if just woke up from VLLS mode */
-    if (PMC_REGSC & PMC_REGSC_ACKISO_MASK) PMC_REGSC |= PMC_REGSC_ACKISO_MASK;
-
-    /* Use the vector table in flash */
-    SCB_VTOR = 0;
+    if (PMC->REGSC & PMC_REGSC_ACKISO_MASK) PMC->REGSC |= PMC_REGSC_ACKISO_MASK;
 
     /*
      * Start in FEI mode
      */
 
     /* Enable capacitors for crystal */
-    OSC0_CR = OSC_CR_SC8P_MASK | OSC_CR_SC2P_MASK;
+    OSC->CR = OSC_CR_SC8P_MASK | OSC_CR_SC2P_MASK;
 
     /* Enable OSC, 8-32 MHz range, low power mode */
-    MCG_C2 = MCG_C2_RANGE0(2) | MCG_C2_EREFS0_MASK;
+    MCG->C2 = MCG_C2_RANGE0(2) | MCG_C2_EREFS0_MASK;
 
     /* Switch to crystal as clock source, FLL input of 16 MHz / 512 */
-    MCG_C1 = MCG_C1_CLKS(2) | MCG_C1_FRDIV(4);
+    MCG->C1 = MCG_C1_CLKS(2) | MCG_C1_FRDIV(4);
 
     /* Wait for crystal oscillator to begin */
-    while ((MCG_S & MCG_S_OSCINIT0_MASK) == 0);
+    while ((MCG->S & MCG_S_OSCINIT0_MASK) == 0);
 
     /* Wait for the FLL to use the oscillator */
-    while ((MCG_S & MCG_S_IREFST_MASK) != 0);
+    while ((MCG->S & MCG_S_IREFST_MASK) != 0);
 
     /* Wait for the MCGOUTCLK to use the oscillator */
-    while ((MCG_S & MCG_S_CLKST_MASK) != MCG_S_CLKST(2));
+    while ((MCG->S & MCG_S_CLKST_MASK) != MCG_S_CLKST(2));
 
     /*
      * Now in FBE mode
      */
 
     /* Config PLL input for 4 MHz (16 MHz crystal / 4) */
-    MCG_C5 = MCG_C5_PRDIV0(3);
+    MCG->C5 = MCG_C5_PRDIV0(3);
 
     /* Config PLL for 96 MHz output */
-    MCG_C6 = MCG_C6_PLLS_MASK | MCG_C6_VDIV0(0);
+    MCG->C6 = MCG_C6_PLLS_MASK | MCG_C6_VDIV0(0);
 
     /* Wait for PLL to start using crystal as its input */
-    while (!(MCG_S & MCG_S_PLLST_MASK));
+    while (!(MCG->S & MCG_S_PLLST_MASK));
 
     /* Wait for PLL to lock */
-    while (!(MCG_S & MCG_S_LOCK0_MASK));
+    while (!(MCG->S & MCG_S_LOCK0_MASK));
 
     /*
      * Now in PBE mode
      */
 
     /* Clock config divisors: 48 MHz core, 48 MHz bus, 24 MHz flash */
-    SIM_CLKDIV1 = SIM_CLKDIV1_OUTDIV1(1) |
-                  SIM_CLKDIV1_OUTDIV2(1) |
-                  SIM_CLKDIV1_OUTDIV4(3);
+    SIM->CLKDIV1 = SIM_CLKDIV1_OUTDIV1(1) |
+                   SIM_CLKDIV1_OUTDIV2(1) |
+                   SIM_CLKDIV1_OUTDIV4(3);
 
     /* Switch to PLL as clock source, FLL input of 16 MHz / 512 */
-    MCG_C1 = MCG_C1_CLKS(0) | MCG_C1_FRDIV(4);
+    MCG->C1 = MCG_C1_CLKS(0) | MCG_C1_FRDIV(4);
 
     /* Wait for PLL clock to be used */
-    while ((MCG_S & MCG_S_CLKST_MASK) != MCG_S_CLKST(3));
+    while ((MCG->S & MCG_S_CLKST_MASK) != MCG_S_CLKST(3));
 
     /*
      * Now in PEE mode
      */
 
     /* Configure USB for a 48 MHz clock (96 MHz PLL / 2) */
-    SIM_CLKDIV2 = SIM_CLKDIV2_USBDIV(1);
+    SIM->CLKDIV2 = SIM_CLKDIV2_USBDIV(1);
 
     /* USB uses PLL clock, trace is CPU clock, CLKOUT = OSCERCLK0 */
-    SIM_SOPT2 = SIM_SOPT2_USBSRC_MASK |
+    SIM->SOPT2 = SIM_SOPT2_USBSRC_MASK |
                 SIM_SOPT2_PLLFLLSEL_MASK |
                 SIM_SOPT2_TRACECLKSEL_MASK |
                 SIM_SOPT2_CLKOUTSEL(6);
-#endif
 }
 
 /** @} */
